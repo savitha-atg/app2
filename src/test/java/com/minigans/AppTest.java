10a90e9570a600f0e048ff3670961f0526e197f7
Savitha Ganapathi <savitha@acrisuretechnology.com>
2022-03-25 14:13:14 -0500

    first commit

---
+++ src/test/java/com/minigans/AppTest.java
@@ -0,0 +1,20 @@
+package com.minigans;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
